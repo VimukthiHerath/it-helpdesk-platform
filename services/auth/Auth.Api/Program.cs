b34a1f6f9ca9d768beaebca35016c94775e4cce5
IT24101503 <it24101503@my.sliit.lk>
2026-08-19 19:29:49 +0530

    Added: Controller and repositories created to the App builder and mapped the controllers

--- services/auth/Auth.Api/Program.cs
+++ services/auth/Auth.Api/Program.cs
@@ -1,7 +1,11 @@
+using Auth.Api.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<UserRepository>();
 
 var app = builder.Build();
 
@@ -11,6 +15,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapControllers();
 app.MapGet("/health", () => Results.Ok(new { status = "healthy", service = "Auth" }));
 //Test comment 
 app.Run();
