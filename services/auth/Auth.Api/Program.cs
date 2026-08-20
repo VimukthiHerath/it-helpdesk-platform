df295ee325f28eceea84dd8121d06e41e666f8ed
IT24101503 <it24101503@my.sliit.lk>
2026-08-20 20:26:09 +0530

    Modified: Builder configuration to add ApplicationDbContext instead of MySQL.Data

--- services/auth/Auth.Api/Program.cs
+++ services/auth/Auth.Api/Program.cs
@@ -1,11 +1,16 @@
-using Auth.Api.Repository;
+using Microsoft.EntityFrameworkCore;
+using Auth.Api.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("AuthDb"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("AuthDb"))));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<UserRepository>();
 
 var app = builder.Build();
 
