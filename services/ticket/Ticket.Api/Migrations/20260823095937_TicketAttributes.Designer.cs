f6776f4c1d82c9783603c0ecc20179947ac0effe
IT24101503 <it24101503@my.sliit.lk>
2026-08-23 15:42:04 +0530

    DB-Migration: Performed Migration of the new ticket class

--- services/ticket/Ticket.Api/Migrations/20260823095937_TicketAttributes.Designer.cs
+++ services/ticket/Ticket.Api/Migrations/20260823095937_TicketAttributes.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Ticket.Api.Data;
 
@@ -10,9 +11,11 @@ using Ticket.Api.Data;
 namespace Ticket.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260823095937_TicketAttributes")]
+    partial class TicketAttributes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
